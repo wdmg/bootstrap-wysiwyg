a3f0cafd23fe2ee80ebfdeac8847358844abac50
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-04-14 22:33:50 +0300

    Added label and tooltip translations

--- build/wysiwyg.js
+++ build/wysiwyg.js
@@ -62,6 +62,8 @@
             symbolsDefault: ["&lt;", "&gt;", "&laquo;", "&raquo;", "&lsaquo;", "&rsaquo;", "&quot;", "&prime;", "&Prime;", "&lsquo;", "&rsquo;", "&sbquo;", "&ldquo;", "&rdquo;", "&bdquo;", "&#10076;", "&#10075;", "&amp;", "&apos;", "&sect;", "&copy;", "&not;", "&reg;", "&macr;", "&deg;", "&plusmn;", "&sup1;", "&sup2;", "&sup3;", "&frac14;", "&frac12;", "&frac34;", "&acute;", "&micro;", "&para;", "&middot;", "&iquest;", "&fnof;", "&trade;", "&bull;", "&hellip;", "&oline;", "&ndash;", "&mdash;", "&permil;", "&#125;", "&#123;", "&#61;", "&ne;", "&cong;", "&asymp;", "&le;", "&ge;", "&ang;", "&perp;", "&radic;", "&sum;", "&int;", "&#8251;", "&divide;", "&infin;", "&#64;", "&#91;", "&#93;", "&larr;", "&uarr;", "&rarr;", "&darr;", "&harr;", "&crarr;", "&lArr;", "&uArr;", "&rArr;", "&dArr;", "&hArr;", "&#10144;", "&#10148;", "&#10149;", "&#10150;", "&#10163;", "&#8634;", "&#8635;", "&#8679;", "&#8617;", "&#11015;", "&#11014;", "&spades;", "&clubs;", "&hearts;", "&diams;", "&#9825;", "&#9826;", "&#9828;", "&#9831;", "&#8372;", "&euro;", "&dollar;", "&cent;", "&pound;", "&#8381;", "&yen;", "&#8377;", "&#22291;", "&#8376;"],
             colorPalette: [["rgb(0, 0, 0)","rgb(67, 67, 67)","rgb(102, 102, 102)","rgb(153, 153, 153)","rgb(183, 183, 183)","rgb(204, 204, 204)","rgb(217, 217, 217)","rgb(239, 239, 239)","rgb(243, 243, 243)","rgb(255, 255, 255)"],["rgb(152, 0, 0)","rgb(255, 0, 0)","rgb(255, 153, 0)","rgb(255, 255, 0)","rgb(0, 255, 0)","rgb(0, 255, 255)","rgb(74, 134, 232)","rgb(0, 0, 255)","rgb(153, 0, 255)","rgb(255, 0, 255)"],["rgb(230, 184, 175)","rgb(244, 204, 204)","rgb(252, 229, 205)","rgb(255, 242, 204)","rgb(217, 234, 211)","rgb(208, 224, 227)","rgb(201, 218, 248)","rgb(207, 226, 243)","rgb(217, 210, 233)","rgb(234, 209, 220)","rgb(221, 126, 107)","rgb(234, 153, 153)","rgb(249, 203, 156)","rgb(255, 229, 153)","rgb(182, 215, 168)","rgb(162, 196, 201)","rgb(164, 194, 244)","rgb(159, 197, 232)","rgb(180, 167, 214)","rgb(213, 166, 189)","rgb(204, 65, 37)","rgb(224, 102, 102)","rgb(246, 178, 107)","rgb(255, 217, 102)","rgb(147, 196, 125)","rgb(118, 165, 175)","rgb(109, 158, 235)","rgb(111, 168, 220)","rgb(142, 124, 195)","rgb(194, 123, 160)","rgb(166, 28, 0)","rgb(204, 0, 0)","rgb(230, 145, 56)","rgb(241, 194, 50)","rgb(106, 168, 79)","rgb(69, 129, 142)","rgb(60, 120, 216)","rgb(61, 133, 198)","rgb(103, 78, 167)","rgb(166, 77, 121)","rgb(133, 32, 12)","rgb(153, 0, 0)","rgb(180, 95, 6)","rgb(191, 144, 0)","rgb(56, 118, 29)","rgb(19, 79, 92)","rgb(17, 85, 204)","rgb(11, 83, 148)","rgb(53, 28, 117)","rgb(116, 27, 71)","rgb(91, 15, 0)","rgb(102, 0, 0)","rgb(120, 63, 4)","rgb(127, 96, 0)","rgb(39, 78, 19)","rgb(12, 52, 61)","rgb(28, 69, 135)","rgb(7, 55, 99)","rgb(32, 18, 77)","rgb(76, 17, 48)"]],
             mode: 'editor',
+            language: 'en-us',
+            translations: {},
             debug: true
         };
 
@@ -113,7 +115,6 @@
 
                 // Build toolbar by config
                 if(typeof (_this._config.toolbar) == 'object') {
-
                     $.each(_this._config.toolbar, function (index, elem) {
 
                         var $toolbar = $('<div class="btn-group" role="group" aria-label="..." />');
@@ -341,7 +342,6 @@
 
                 // Set behavior for toolbar buttons
                 if(_this._$toolbar.length) {
-
                     _this._$toolbar.on('click', '[data-action]', function(event) {
                         var $target = $(event.currentTarget);
                         var action = $target.data('action');
@@ -667,7 +667,6 @@
                         }
 
                     });
-
                 }
 
                 // On selected content
@@ -992,6 +991,38 @@
                         this._popoverIsVisible = false;
                     }
                 },
+                _detectLanguage: {
+                    value: function detectLanguage() {
+
+                        var language = null;
+                        if (navigator.languages && navigator.languages.length) {
+                            language = navigator.languages[0];
+                        } else {
+                            language = navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en';
+                        }
+
+                        return language.toLowerCase();
+
+                    }
+                },
+                _translate: {
+                    value: function translate(string) {
+
+                        var _this = this;
+                        var language = _this._config.language;
+
+                        if (typeof (language) === "undefined")
+                            language = _this._detectLanguage();
+
+                        if (_this._config.translations.hasOwnProperty(language)) {
+                            if (_this._config.translations[language][string.toString()]) {
+                                string = _this._config.translations[language][string.toString()];
+                            }
+                        }
+
+                        return string.toLocaleString();
+                    }
+                },
                 _buildTollbarButton: {
                     value: function buildTollbarButton(action, value, icon, hotkey, tooltip, content) {
 
@@ -1013,7 +1044,7 @@
                                 html: true,
                                 placement: 'top',
                                 container: 'body',
-                                title: tooltip.toString().trim()
+                                title: _this._translate(tooltip.toString().trim())
                             });
                         }
 
@@ -1166,7 +1197,7 @@
                             $dropdownBtn.tooltip({
                                 html: true,
                                 placement: 'top',
-                                title: tooltip.toString().trim()
+                                title: this._translate(tooltip.toString().trim())
                             });
                         }
 
